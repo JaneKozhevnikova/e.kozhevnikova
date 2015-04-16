f000aa168728116ef83235e1b8c307cf17f4bb04
JaneKozhevnikova <multicolore10@gmail.com>
2015-04-16 22:04:31 +0900

    homework_6.04

---
+++ locators1.py
@@ -0,0 +1,7 @@
+from selenium.webdriver.common.by import By
+
+class MainPageLocators(object):
+    GO_BUTTON = (By.XPATH, '/html/body/table/tbody/tr[2]/td/form/table/tbody/tr/td/div/button')
+
+class SearchResultsPageLocators(object):
+    pass
