5fc818e88ccecf864e87192b6351af380539021a
JaneKozhevnikova <multicolore10@gmail.com>
2015-04-17 01:54:41 +0900

    Update unittest_for_webpage_setup-teardown.py

--- 6.04/unittest_for_webpage_setup-teardown.py
+++ 6.04/unittest_for_webpage_setup-teardown.py
@@ -5,9 +5,6 @@ from unittest import TestCase
 # driver = webdriver.Remote(desired_capabilities=webdriver.DesiredCapabilities.FIREFOX)
 #driver.get("https://ya.ru")
 from selenium.common.exceptions import NoSuchElementException
-from selenium_test_web_page import driver
-
-
 
 class TestString(unittest.TestCase):
 
@@ -18,22 +15,22 @@ class TestString(unittest.TestCase):
         self.driver.get("http://ya.ru")
         self.driver.implicitly_wait(60)
 
-        inputElement = driver.find_element_by_name("text")
+        inputElement = self.driver.find_element_by_name("text")
         inputElement.send_keys("Apps for Android")
         inputElement.submit()
-        print(driver.title)
-        self.assertIn("Apps for Android", driver.title)
+        print(self.driver.title)
+        self.assertIn("Apps for Android", self.driver.title)
 
     def test1(self):
-        
+
         self.driver.get("http://translate.ya.ru")
         self.driver.implicitly_wait(60)
 
-        driver.find_element_by_id("cmdAuto").click()
-        inputElement = driver.find_element_by_id("srcText")
+        self.driver.find_element_by_id("cmdAuto").click()
+        inputElement = self.driver.find_element_by_id("srcText")
         inputElement.send_keys("Yandex")
         try:
-            driver.find_elements_by_id("cmdSubmit")
+            self.driver.find_elements_by_id("cmdSubmit")
             #check = True
         except NoSuchElementException:
            #check = False
